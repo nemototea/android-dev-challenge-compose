ea50f03cc52c21587a21a51f6c253b15339323ee
nemototea <nemototea@gmail.com>
2021-02-27 19:56:33 +0900

    implement navigation compose

--- app/src/main/java/com/example/androiddevchallenge/main/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/main/MainActivity.kt
@@ -19,11 +19,20 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -32,32 +41,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                NavGraph(viewModel)
             }
         }
     }
 }
 
-// Start building your app here!
 @Composable
-fun MyApp() {
+fun NavGraph(viewModel: MainActivityViewModel) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") { MainScreen(viewModel, navController) }
+        composable(
+            "detail/{id}",
+            arguments = listOf(navArgument("id") { defaultValue = 0 })
+        ) {
+            DetailScreen(
+                id = it.arguments?.getInt("id", 0) ?: throw Exception("Invalid value.")
+            )
+        }
+    }
+}
+
+@Composable
+fun MainScreen(viewModel: MainActivityViewModel, navController: NavHostController) {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Card {
+                Text(text = "Ready... Set... GO!")
+                Text(text = "Ready... Set... GO!")
+                Text(text = "Ready... Set... GO!")
+            }
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
-    }
-}
+fun DetailScreen(id: Int) {
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
-    }
 }
